9d8b0c709400c31d91aeefb0f99d578ab7f38fd0
YanivHacker1 <95615845+YanivHacker1@users.noreply.github.com>
2022-07-12 18:30:10 +0300

    dao changes + put maps in comment

--- app/src/main/java/com/example/androidstudioproject/entities/UserConnections.java
+++ app/src/main/java/com/example/androidstudioproject/entities/UserConnections.java
@@ -1,32 +1,37 @@
 package com.example.androidstudioproject.entities;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 
 @Entity(primaryKeys = {"userEmail", "secondUserEmail"})
 public class UserConnections {
+    @NonNull
     private String userEmail;
+    @NonNull
     private String secondUserEmail;
 
-    public UserConnections(String userEmail, String secondUserEmail) {
+    public UserConnections(@NonNull String userEmail, @NonNull String secondUserEmail) {
         this.userEmail = userEmail;
         this.secondUserEmail = secondUserEmail;
     }
 
     //getters
+    @NonNull
     public String getUserEmail() {
         return userEmail;
     }
 
+    @NonNull
     public String getSecondUserEmail() {
         return secondUserEmail;
     }
 
     //setters
-    public void setUserEmail(String userEmail) {
+    public void setUserEmail(@NonNull String userEmail) {
         this.userEmail = userEmail;
     }
 
-    public void setSecondUserEmail(String secondUserEmail) {
+    public void setSecondUserEmail(@NonNull String secondUserEmail) {
         this.secondUserEmail = secondUserEmail;
     }
 }
